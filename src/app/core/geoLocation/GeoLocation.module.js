5075dc3a38b6c10ab029cb4d7abb5e9c9ac9c96c
saibs <sai.b.satchitanand@gmail.com>
2016-07-03 23:23:44 +0100

    folder rename issue

--- src/app/core/geoLocation/GeoLocation.module.js
+++
@@ -1,9 +0,0 @@
-import angular from 'angular';
-
-import GeoLocationService from './geoLocation.service';
-
-const GeoLocationServiceModule = angular.module('app.geoLocationService', [
-])
-	.service('GeoLocationService', GeoLocationService);
-
-export default GeoLocationServiceModule;
